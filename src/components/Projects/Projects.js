87c36437dd69ccd31e9d92b625628b3c9b048d07
joshwilcken <joshwilcken@gmail.com>
2017-09-04 20:43:03 -0600

    committing updates

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './projects.css';
 
-
-
 class Projects extends Component {
     render() {
         return (
-            <div>
+            <div className='projectsWrapper'>
                 <h1 className='projectTitle'>Projects</h1>
                 <div className='projects'>
                     <div className='imageContainer'>
@@ -20,7 +18,7 @@ class Projects extends Component {
                                     <li>PostgreSQL</li>
                                 </ul>
                             </div>
-                            <a href="https://github.com/CloneWellsFargoChase/groupProject/blob/master/ReadMe.md"><div className='hoverTextLeft'>Site</div></a>
+                            <a href="https://chase-clone.herokuapp.com/"><div className='hoverTextLeft'>Site</div></a>
                             <a href="https://www.youtube.com/watch?v=n25mskTcKIg&amp;t=7s"><div className='hoverTextRight'>Video Tour</div></a>
                         </div>
                     </div>
@@ -37,6 +35,21 @@ class Projects extends Component {
                             </div>
                             <a href="http://papertradeapp.herokuapp.com/"><div className='hoverTextLeft'>Site</div></a>
                             <a href="https://www.youtube.com/watch?v=6zIsZ-vQ4xY&amp;t=5s"><div className='hoverTextRight'>Video Tour</div></a>
+                        </div>
+                    </div>
+                    <div className='imageContainer'>
+                        <img className='hoverImage'src={require("../../Images/YoutubeClone.png")} alt="Youtube Clone"/>
+                        <div className='overlay'>
+                            <h3 className='hoverTextTop'>Youtube Clone</h3>
+                            <div className='hoverTextList'>
+                                <ul>
+                                    <li>React</li>
+                                    <li>BootstrapUI</li>
+                                    <li>HTML/CSS</li>
+                                </ul>
+                            </div>
+                            <a href="http://joshwilcken-youtubelone.surge.sh/"><div className='hoverTextLeft'>Site</div></a>
+                            <a href="https://github.com/joshwilcken/youtubeClone"><div className='hoverTextRight'>GitHub Repo</div></a>
                         </div>
                     </div>
                 </div>
